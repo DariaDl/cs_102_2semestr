0f8e90866705f76c93402d3e0a7d82ab6ee7a682
Daria <D89110030406@gmail.com>
2021-06-19 14:47:52 +0300

    first

--- homework06/bayes.py
+++ homework06/bayes.py
@@ -1,17 +1,82 @@
-class NaiveBayesClassifier:
+from collections import Counter
+import math
 
-    def __init__(self, alpha):
-        pass
+
+
+class NaiveBayesClassifier:
+    def __init__(self, alpha: float = 1e-5):
+        self.alpha = alpha
+        self.mockup: dict = {
+            "labels": {},
+            "words": {},
+        }
 
     def fit(self, X, y):
-        """ Fit Naive Bayes classifier according to X, y. """
-        pass
+        """ Fit Naive Bayes classifier according to X, y."""
+        collection = []
+        for title, tag in zip(X, y):
+            for word in title.split():
+                couple = (word, tag)
+                collection.append(couple)
+
+        self.uncommon_words = Counter(collection)
+        print(f"uncommon_words", {self.uncommon_words})
+
+        self.count_dictionary = dict(Counter(y))
+        print(f"count_dictionary", {self.count_dictionary})
+
+        words = [word for title in X for word in title.split()]
+        self.count_words = dict(Counter(words))
+        print(f"count_words", {self.count_words})
+
+        self.mockup = {
+            "labels": {},
+            "words": {},
+        }
+
+        for publish in self.count_dictionary:
+            counter = 0
+            for word, tag_name in self.uncommon_words:
+                if publish == tag_name:
+                    counter += self.uncommon_words[(word, publish)]
+            c_d = self.counted_dict[publish]
+            params = {
+                "tag_count": counter,
+                "probability": c_d / len(y),
+            }
+            self.mockup["tags"][publish] = params
+
+        for word in self.count_words:
+            params = {}
+            for publish in self.count_dict:
+                vk = self.mockup["tags"][publish]["tags_count"]
+                vik = self.uncommon_words.get((word, publish), 0)
+                lenght = len(self.count_words)
+                alpha = self.alpha
+                smooth = (vik + alpha) / (vk + vik * lenght)
+                params[publish] = smooth
+            self.mockup["words"][word] = params
 
     def predict(self, X):
         """ Perform classification on an array of test vectors X. """
-        pass
+        words = X.split()
+        fate = []
+        for current_tag in self.mockup["labels"]:
+            probability = self.mockup["labels"][current_tag]["probability"]
+            total = math.log(probability, math.e)
+            for word in words:
+                word_dict = self.mockup["words"].get(word, None)
+                if word_dict:
+                    total += math.log(word_dict[current_tag], math.e)
+            fate.append((total, current_tag))
+        _, forecast = max(fate)
+        return forecast
 
     def score(self, X_test, y_test):
         """ Returns the mean accuracy on the given test data and labels. """
-        pass
+        correct = []
+        for element in X_test:
+            correct.append(self.predict(element))
+        final = sum(0 if correct[i] != y_test[i] else 1 for i in range(len(X_test))) / len(X_test)
 
+        return final
--- homework06/db.py
+++ homework06/db.py
@@ -1,16 +1,15 @@
-from sqlalchemy import Column, String, Integer
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String, create_engine  # type: ignore
+from sqlalchemy.orm import sessionmaker  # type: ignore
+from sqlalchemy.ext.declarative import declarative_base  # type: ignore
+
+from scraputils import get_news # type: ignore
+Base = declarative_base()  # type: ignore
+engine = create_engine("sqlite:///news.db")  # type: ignore
+session = sessionmaker(bind=engine)  # type: ignore
 
 
-Base = declarative_base()
-engine = create_engine("sqlite:///news.db")
-session = sessionmaker(bind=engine)
-
-
-class News(Base):
-    __tablename__ = "news"
+class News(Base):  # type: ignore
+    __tablename__ = "news"  # type: ignore
     id = Column(Integer, primary_key=True)
     title = Column(String)
     author = Column(String)
@@ -19,4 +18,20 @@ class News(Base):
     points = Column(Integer)
     label = Column(String)
 
+
 Base.metadata.create_all(bind=engine)
+
+if __name__ == "__main__":
+    k = session()
+    news_list = get_news("https://news.ycombinator.com/newest", n_pages=35)
+    for element in range(len(news_list)):
+        news = News(
+            title=news_list[element]["title"],
+            author=news_list[element]["author"],
+            url=news_list[element]["url"],
+            comments=news_list[element]["comments"],
+            points=news_list[element]["points"],
+        )
+        k.add(news)
+        k.commit()
+
